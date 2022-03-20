486e9911a1a57227fff4de333c00676dd354f9c9
Victoria Guillen <victoriaguillen@uma.es>
2022-03-20 11:45:20 +0100

    arreglo instancia y str

--- fuel.py
+++ fuel.py
@@ -1,10 +1,13 @@
+
 class Fuel:
     def __init__(self,engine):
          self.level = 1000
+         self.engine = engine
 
     def update(self):
         self.level = self.level - abs(self.engine.get_speed())*0.01
 
     def __str__(self):
-        status = "Nivel de combustible: " + str(self.update()) + ' L'
+        
+        status = "Nivel de combustible: " + str(self.level) + ' L'
         return status
